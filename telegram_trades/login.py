93c19687b6699a410df8896b10cb774e86eb07cf
Maheswaran Palaniselvan <maheswaran.palaniselvan@gmail.com>
2024-01-07 16:54:43 +0530

    check broker connection

--- telegram_trades/login.py
+++ telegram_trades/login.py
@@ -1,9 +1,16 @@
-
+# pip install pya3
+# https://github.com/jerokpradeep/pya3
+from pya3 import *
+from constants import BRKR
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 def get_broker():
     """
         login and authenticate
         return broker object
     """
-    brkr = "broker object"
+    brkr = Aliceblue(user_id=BRKR['username'],api_key=BRKR["api_key"])
+    _ = brkr.get_session_id()
+    print(brkr.get_profile())
     return brkr
