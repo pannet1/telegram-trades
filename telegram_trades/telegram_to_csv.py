3fd26a8559bc35bb5c1a492f3fd51073d9a53989
Maheswaran Palaniselvan <maheswaran.palaniselvan@gmail.com>
2024-01-08 22:01:18 +0530

    Telegram to csv with reply msgs and stored sessions

---
+++ telegram_trades/telegram_to_csv.py
@@ -0,0 +1,27 @@
+from telethon.sync import TelegramClient, events
+import csv
+from datetime import datetime
+from constants import TGRM
+
+
+api_id = TGRM['api_id']
+api_hash = TGRM['api_hash']
+channel_ids = TGRM['channel_ids']
+
+client = TelegramClient('anon', api_id, api_hash)
+
+replace_non_ascii = lambda s: ''.join(' ' if ord(i) >= 128 or i == '\n' else i for i in s)
+
+@client.on(events.NewMessage(chats=channel_ids))
+async def my_event_handler(event):
+    chat = await event.get_chat()
+    with open(TGRM["output_file"], 'a', encoding='utf-8',  newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        if event.reply_to_msg_id is not None:
+            original_message = await client.get_messages(chat.id, ids=event.reply_to_msg_id)
+            csv_writer.writerow([datetime.now(), chat.title, replace_non_ascii(original_message.raw_text) +" "+ replace_non_ascii(event.raw_text)])
+        else:
+            csv_writer.writerow([datetime.now(), chat.title, replace_non_ascii(event.raw_text)])
+
+client.start(phone=TGRM["phone_number"])
+client.run_until_disconnected()
