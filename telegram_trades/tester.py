634cee5d4dbbeede944f74f4ef670ee951445934
Maheswaran Palaniselvan <maheswaran.palaniselvan@gmail.com>
2024-04-06 16:01:54 +0530

    tester

--- telegram_trades/tester.py
+++ telegram_trades/tester.py
@@ -4,21 +4,21 @@ import pandas as pd
 from telegram_message_parser import PremiumJackpot, SmsOptionsPremium, PaidCallPut, PaidStockIndexOption
 
 
-# df = pd.read_csv(input_file, header=None)
-# for i, row in df.iterrows():
-# #     # print(i, row)
-#     if row[1] == "PREMIUM JACKPOT":
-#         i = PremiumJackpot(row[0], row[2])
-#         i.get_signal()
-#     elif row[1] == "SMS Options Premium":
-#         i = SmsOptionsPremium(row[0], row[2])
-#         i.get_signal()
-#     elif row[1] == "Paid - CALL & PUT":
-#         i = PaidCallPut(row[0], row[2])
-#         i.get_signal()
-#     elif row[1] == "Paid Stock & Index Option":
-#         i = PaidStockIndexOption(row[0], row[2])
-#         i.get_signal()
+df = pd.read_csv(input_file, header=None)
+for i, row in df.iterrows():
+    #     # print(i, row)
+    if row[1] == "PREMIUM JACKPOT":
+        i = PremiumJackpot(row[0], row[2])
+        i.get_signal()
+    elif row[1] == "SMS Options Premium":
+        i = SmsOptionsPremium(row[0], row[2])
+        i.get_signal()
+    elif row[1] == "Paid - CALL & PUT":
+        i = PaidCallPut(row[0], row[2])
+        i.get_signal()
+    elif row[1] == "Paid Stock & Index Option":
+        i = PaidStockIndexOption(row[0], row[2])
+        i.get_signal()
 
 
 # msg = "BANKNIFTY 7 FEB 45800 CE IF CROSSES & SPOT SUSTAIN ONLY ABOVE 253.85 WILL TRY TO HIT TARGETS @ 275 300 330 360 400 & ABOVE$$$$"
@@ -26,9 +26,9 @@ from telegram_message_parser import PremiumJackpot, SmsOptionsPremium, PaidCallP
 # i = SmsOptionsPremium(1707192066, msg)
 # i.get_signal()
 
-msg = "BUY #SENSEX 74100 CE ABOVE -180-190 TARGET- 30/70/100/200/250 Point  SL-120  EXPIRY 5TH APR"
-i = PremiumJackpot(1707192066, msg)
-i.get_signal()
+# msg = "BUY #SENSEX 74100 CE ABOVE -180-190 TARGET- 30/70/100/200/250 Point  SL-120  EXPIRY 5TH APR"
+# i = PremiumJackpot(1707192066, msg)
+# i.get_signal()
 
 # msg = "14th feb expiry  BUY 45200 CE only abv 220 SL-180 TARGET -400-500-750  cmp - 190"
 # msg = "21st feb expiry  BUY 45600 PE only abv 330 SL-290 TARGET -400-500++  cmp - 310"
